c7f3a1dd36df616dc75a130fba0cd51030f8b1a7
ian <iansangines@gmail.com>
2017-04-01 13:38:54 +0200

    First commit

---
+++ API/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class UserProfile(models.Model):
+    # This field is required.
+    user = models.OneToOneField(User)
+
+    # def create_user_profile(sender, instance, created, **kwargs):
+    #     if created:
+    #         UserProfile.objects.create(user=instance)
+
+
+
+class Alumne(UserProfile):
+    dni = models.TextField(blank=False)
+    dispositiu = models.OneToOneField('Dispositiu')
+
+
+class Professor(UserProfile):
+    something = models.TextField()
+
+
+class Dispositiu(models.Model):
+    MAC = models.TextField(blank=False)
+    codi = models.TextField(blank=False)
+
+class Sala(models.Model):
+    MAC = models.TextField(blank=False)
+
+class Classe(models.Model):
+    #hora_inici : models.TimeField
+
