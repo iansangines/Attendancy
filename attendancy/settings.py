ae4523385653dc7d462e2643ceec45f3f017a369
maxbhs <max_barcatmarti@msn.com>
2017-05-06 22:19:12 +0200

    allowed host

--- attendancy/settings.py
+++ attendancy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4p+a_wa&sq(rl8v=jyktqf1znf&%=slayc*)71pezwunsjxoxl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.4.41.154']
+ALLOWED_HOSTS = ['10.4.41.154','localhost']
 
 
 # Application definition
